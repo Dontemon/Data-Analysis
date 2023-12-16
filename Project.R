8d2c0a3cac14ff9ae5a6b43e5ed65ba009d1f109
Knikits <nikita.kostriukov@yandex.ru>
2023-12-16 16:21:06 +0700

    Add data for diamonds and libraries

--- Project.R
+++ Project.R
@@ -1,9 +1,35 @@
+# Библиотека ----
+
+#Пакеты
+#install.packages("shiny")
+#install.packages("corrplot")
+#install.packages("ggplot2")
+#install.packages("randomForest")
+#install.packages("xgboost")
+#install.packages("readr")
+#install.packages("dplyr")
+#install.packages("psych")
+
+#Подключение библиотек
+library(shiny)
+library(corrplot)
+library(ggplot2)
+library(randomForest)
+library(xgboost)
+library(readr)
+library(dplyr)
+library(psych)
+
 # Очистка рабочего пространства ----
+
 while (dev.cur() != 1) {
   dev.off()
 }
 rm(list=ls())
 cat("\014")
 
-library(shiny)
-runExample("01_hello")
+# Загрузка данных ----
+
+setwd("D:/Data-Analysis/") # установка рабочей директории
+diamonds = read.csv("diamonds.csv") # чтение данных из файла
+diamonds # вывод данных в консоль
