2d2ab4505a0356fa1ef7074bc01ef8b60bb42cff
Slava Romanov <slaves2001@gmail.com>
2023-12-23 13:07:02 +0700

    Попытка переписать код в тэгах

--- app.R
+++ app.R
@@ -17,50 +17,110 @@ library(randomForest)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-  titlePanel(h1("Предсказание цены бриллианта")),
+  includeCSS("style.css"),
+  titlePanel(tags$h1("Предсказание цены бриллианта")),
   mainPanel(
-    h4("1) Цена - цена в долларах США"),
-    h4("2) Карат - вес бриллианта"),
-    h4("3) Огранка - качество реза"),
-    h4("4) Цвет - цвет бриллианта"),
-    h4("5) Чистота – показатель того, насколько чист алмаз"),
-    h4("6) Глубина - общий процент глубины"),
-    h4("7) Площадка - самая большая грань бриллианта"),
-    h4("8) х - длина в мм"),
-    h4("9) у - ширина в мм"),width = 
-    h4("10) z - глубина в мм"),
-    numericInput("CaratInput", "Введите количество карат:", value = 0),
-    numericInput("DepthInput", "Введите Depth:", value = 0),
-    numericInput("TableInput", "Введите Table:", value = 0),
-    numericInput("XInput", "Введите ширину:", value = 0),
-    numericInput("YInput", "Введите высоту:", value = 0),
-    numericInput("ZInput", "Введите длину:", value = 0),
-    selectInput("CutValue", "Выберите огранку:",
-                choices = c("Fair", "Good", "Very Good", "Premium", "Ideal")),
-    selectInput("ClarityValue", "Выберите прозрачность:",
-                choices = c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF")),
-    selectInput("ColorValue", "Выберите цвет:",
-                choices = c("D", "E", "F", "G", "H", "I", "J")),
-    actionButton("myButton", "Получить значение"),
-    actionButton("myButton2", "Получить значение2"),
+    tags$table(
+      tags$tr(
+        tags$td("Введите количество карат:"),
+        tags$td(tags$input(id = "caratInput")),
+        tags$td("Введите глубину:"),
+        tags$td(tags$input(id = "depthInput")),
+        tags$td("Введите площадку:"),
+        tags$td(tags$input(id = "tableInput")),
+        tags$td("Введите ширину:"),
+        tags$td(tags$input(id = "xInput")),
+        tags$td("Введите высоту:"),
+        tags$td(tags$input(id = "yInput")),
+      ),
+      tags$tr(
+        tags$td("Введите длину:"),
+        tags$td(tags$input(id = "zInput")),
+        tags$td("Выберите огранку:"),
+        tags$td(tags$select(
+          id = "cutValue",
+          tags$option("Fair"),
+          tags$option("Good"),
+          tags$option("Very Good"),
+          tags$option("Premium"),
+          tags$option("Ideal"),
+        )),
+        tags$td("Выберите прозрачность:"),
+        tags$td(tags$select(
+          id = "clarityValue",
+          tags$option("I1"),
+          tags$option("SI2"),
+          tags$option("SI1"),
+          tags$option("VS2"),
+          tags$option("VS1"),
+          tags$option("VVS2"),
+          tags$option("VVS1"),
+          tags$option("IF"),
+        )),
+        tags$td("Выберите цвет:"),
+        tags$td(tags$select(
+          id = "colorValue",
+          tags$option("D"),
+          tags$option("E"),
+          tags$option("F"),
+          tags$option("G"),
+          tags$option("H"),
+          tags$option("I"),
+          tags$option("J"),
+        )),
+      ),
+    ),
+    tags$div(
+      tags$button(id = "firstButton", "First"),
+      tags$button(id = "secondButton", "Second"),
+      tags$button(id = "thirdButton", "Third"),
+      tags$button(id = "fourthButton", "Fourth"),
+    ),
+    tags$article(
+      tags$div(class ="text","1) Цена - цена в долларах США"),
+      tags$div(class ="text","2) Карат - вес бриллианта"),
+      tags$div(class ="text","3) Огранка - качество реза"),
+      tags$div(class ="text","4) Цвет - цвет бриллианта"),
+      tags$div(class ="text","5) Чистота – показатель того, насколько чист алмаз"),
+      tags$div(class ="text","6) Глубина - общий процент глубины"),
+      tags$div(class ="text","7) Площадка - самая большая грань бриллианта"),
+      tags$div(class ="text","8) х - длина в мм"),
+      tags$div(class ="text","9) у - ширина в мм"),
+      tags$div(class ="text","z - глубина в мм"),
+    ),
+    uiOutput("output"),
+    #numericInput("CaratInput", "Введите количество карат:", value = 0),
+    #numericInput("DepthInput", "Введите глубина:", value = 0),
+    #numericInput("TableInput", "Введите площадку:", value = 0),
+   # numericInput("XInput", "Введите ширину:", value = 0),
+  #  numericInput("YInput", "Введите высоту:", value = 0),
+   # numericInput("ZInput", "Введите длину:", value = 0),
+   # selectInput("CutValue", "Выберите огранку:",
+   #             choices = c("Fair", "Good", "Very Good", "Premium", "Ideal")),
+   # selectInput("ClarityValue", "Выберите прозрачность:",
+    #            choices = c("I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF")),
+   # selectInput("ColorValue", "Выберите цвет:",
+    #            choices = c("D", "E", "F", "G", "H", "I", "J")),
+    #actionButton("myButton", "Получить значение"),
+    #actionButton("myButton2", "Получить значение2"),
     #img(src = "F:/Магистратура_1_семестр/Data-Analysis/diam.jpg", width = "300px", height = "200px"),
-    textOutput("output")
+  actionButton("myButton", "Получить значение"),
   )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-  observeEvent(input$myButton,  {
-    selectedCarat <- input$CaratInput
-    selectedDepth <- input$DepthInput
-    selectedTable <- input$TableInput
-    selectedX <- input$XInput
-    selectedY <- input$YInput
-    selectedZ <- input$ZInput
-    selectedCut <- input$CutValue
-    selectedClarity <- input$ClarityValue
-    selectedColor <- input$ColorValue
+server <- function(input, output, session ) {
+  
+  observeEvent(input$firstButton,  {
+    selectedCarat <- input$caratInput
+    selectedDepth <- input$depthInput
+    selectedTable <- input$tableInput
+    selectedX <- input$xInput
+    selectedY <- input$yInput
+    selectedZ <- input$zInput
+    selectedCut <- input$cutValue
+    selectedClarity <- input$clarityValue
+    selectedColor <- input$colorValue
     
     means = read.xlsx("means.xlsx", sheetIndex = 1)
     sds = read.xlsx("sds.xlsx", sheetIndex = 1)
@@ -87,29 +147,24 @@ server <- function(input, output) {
     
     
     # Используйте значение как угодно, например, выводим его в текстовом поле
-    output$output <- renderText({
-      paste("Цена бриллианта: ", round(prediction), "$")
+    output$output <- renderUI({
+      tags$div(
+        style = "font-size: 18px; color: blue;",
+        paste("Цена бриллианта: ", result, "$")
+      )
     })
   })
   
-  observeEvent(input$myButton2,  {
-    selectedCarat <- input$CaratInput
-    selectedDepth <- input$DepthInput
-    selectedTable <- input$TableInput
-    selectedX <- input$XInput
-    selectedY <- input$YInput
-    selectedZ <- input$ZInput
-    selectedCut <- input$CutValue
-    selectedClarity <- input$ClarityValue
-    selectedColor <- input$ColorValue
-    
-    
+  observeEvent(input$myButton,  {
+    selectedCarat <- input$caratInput
     # Используйте значение как угодно, например, выводим его в текстовом поле
-    output$output <- renderText({
-      paste("Выбрано число:", selectedDepth)
+    output$output <- renderUI({
+      tags$div(
+        style = "font-size: 18px; color: blue;",
+        paste("Цена бриллианта: ", selectedCarat, "$")
+      )
     })
   })
-  
 }
 
 # Run the application 
